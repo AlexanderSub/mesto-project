b3a800e881af5b012c6c4900e13f98f3be67a661
AlexanderSub <subbotin_aleks@mail.ru>
2022-01-30 10:33:19 +0300

    removed unnecessary file

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import './pages/index.js'
-import './components/Popup.js'
-import './components/FormValidator.js'
-import './components/Card.js'
-import './utils/utils.js'
-import './components/Api.js'
-import './utils/constants.js'
-
