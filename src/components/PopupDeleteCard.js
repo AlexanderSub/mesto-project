f8b67fcf18fcf98a58186350d2c7a6784312ab64
Sevastian Dvortsov <86145076+dvortsovs@users.noreply.github.com>
2022-02-04 13:13:11 +0300

    feat: add PopupDeleteCard

---
+++ src/components/PopupDeleteCard.js
@@ -0,0 +1,24 @@
+import Popup from "./Popup.js";
+
+export default class PopupDeleteCard extends Popup {
+  constructor({ popupType, handleFormSubmit }) {
+    super(popupType);
+    this._handleFormSubmit = handleFormSubmit;
+    this._form = this._popup.querySelector('.popup_delete-card-form')
+  }
+
+  _openPopup() {
+    super._openPopup();
+    this._setEventListeners();
+  }
+
+  _setEventListeners() {
+    this._form.addEventListener('submit', this._handleFormSubmit);
+    super._setEventListeners();
+  }
+
+  _closePopup() {
+    this._form.removeEventListener('submit', this._handleFormSubmit);
+    super._closePopup();
+  }
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -21,6 +21,7 @@ import Section from "../components/Section";
 import PopupWithImage from "../components/PopupWithImage";
 import MyCard from "../components/MyCard.js";
 import OtherUserCard from "../components/OtherUserCard.js";
+import PopupDeleteCard from "../components/PopupDeleteCard.js";
 
 export let userId
 
@@ -118,17 +119,24 @@ Promise.all([api.getUserData(), api.getInitialCards()])
   userAvatar.src = userData.avatar
   userNameInput.value = userData.name
   userAboutInput.value = userData.about
+
   const newCards = new Section({data: cards, renderer: (item) => {
-    // console.log(userId, item.owner._id);
-    // if (userId === item.owner._id) {
-    //   console.log('vaflya');
-    // }
       const newCard = (userId === item.owner._id) ? new MyCard(item, '.card__template', () => {
         const popup = new PopupWithImage('.popup_place-picture');
         popup._openPopup(newCard.link, newCard.name);
       }, () => {
-        api.deleteCard(item._id)
-          .then((res) => newCard.deleteCard())
+        const deletePopup = new PopupDeleteCard(
+          { popupType: '.popup_delete-card',
+            handleFormSubmit: (evt) => {
+            evt.preventDefault();
+              api.deleteCard(item._id)
+                .then(() => {
+                  newCard.deleteCard()
+                  deletePopup._closePopup();
+                })
+            }
+          });
+        deletePopup._openPopup();
       }) : new OtherUserCard(item, '.card__template', () => {
         const popup = new PopupWithImage('.popup_place-picture');
         popup._openPopup(newCard.link, newCard.name);
